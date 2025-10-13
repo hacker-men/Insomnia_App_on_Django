8762ede46395691055b2e418d08f7a2e0d2c3f71
hacker-men <deleverkhanovsaidkhon@gmail.com>
2025-10-13 11:54:31 +0500

    Register additional models in admin for enhanced management

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, SleepLog, Tip
 
+admin.site.register(Profile)
+admin.site.register(SleepLog)
+admin.site.register(Tip)
 # Register your models here.

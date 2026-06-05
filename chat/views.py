344820687b655bb381faa08d4099b8274ea8f4dc
hacker-men <saidhondeleverhanov@gmail.com>
2026-06-05 15:20:41 +0500

    Fix chat view template path to render messages correctly

--- chat/views.py
+++ chat/views.py
@@ -3,4 +3,4 @@ from chat.models import Message
 
 def chat_view(request):
     messages = Message.objects.all()[:20]
-    return render(request, "chat/index.html", {"messages": messages})
+    return render(request, "index.html", {"messages": messages})
